35dc66e37a445c6fefc3aebc089bdebaaa2d308b
Ira S <iryna.shevchenko2@gmail.com>
2024-03-08 21:01:14 +0200

    removed unnecessary imports

--- src/pages/userUtilities/ShoppingCartPage/ShoppingCartPage.tsx
+++ src/pages/userUtilities/ShoppingCartPage/ShoppingCartPage.tsx
@@ -1,18 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import css from "./ShoppingCartPage.module.css";
 
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../../redux/store";
 import { useSelector } from "react-redux";
-import { selectCart, selectTotalPrice } from "../../../redux/stores/selectors";
+import { selectCart } from "../../../redux/stores/selectors";
 
 import OrderForm from "../../../components/ShoppingCartForm/ShoppingCartForm";
 import CartList from "../../../components/CartList/CartList";
 
 const ShoppingCartPage = () => {
-  const dispatch = useDispatch<AppDispatch>();
   const cartItems = useSelector(selectCart);
-  const totalPrice = useSelector(selectTotalPrice);
 
   return (
     <section className={css.container}>
